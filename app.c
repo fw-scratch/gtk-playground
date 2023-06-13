fed6d85b792222918365e9ba567295adc72205f1
Florian Wilhelm <fwilhelm.wgt+github@gmail.com>
2023-06-13 22:59:04 +0200

    draft simple spreadsheet

--- app.c
+++ app.c
@@ -1,55 +1,28 @@
 #include <gtk/gtk.h>
 
-int counter = 0;
-GtkWidget *label;
-
-
-static void
-print_hello(GtkWidget *widget,
-            gpointer data)
-{
-  const char *s = gtk_button_get_label(GTK_BUTTON(widget));
-  if (g_strcmp0 (s, "+") == 0) {
-    counter = counter + 1;
-  } else if (g_strcmp0 (s, "-") == 0) {
-    counter = counter - 1;
-  }
-  g_print(s);
-  char str[4];
-  sprintf(str, "%d", counter);
-  g_print(str);
-  gtk_label_set_label(GTK_LABEL(label), str);
-}
-
 static void
 activate(GtkApplication *app,
          gpointer user_data)
 {
   GtkWidget *window;
-  GtkWidget *buttonPlus;
-  GtkWidget *buttonMinus;
-  GtkWidget *box;
+  GtkWidget *grid;
 
   window = gtk_application_window_new(app);
   gtk_window_set_title(GTK_WINDOW(window), "Window");
-  gtk_window_set_default_size(GTK_WINDOW(window), 800, 800);
+  gtk_window_set_default_size(GTK_WINDOW(window), 800, 400);
 
-  box = gtk_box_new(GTK_ORIENTATION_VERTICAL, 5);
-  gtk_box_set_homogeneous(GTK_BOX(box), TRUE);
-  gtk_window_set_child(GTK_WINDOW(window), box);
+  grid = gtk_grid_new();
 
-  buttonPlus = gtk_button_new_with_label("+");
-  g_signal_connect(buttonPlus, "clicked", G_CALLBACK(print_hello), NULL);
-
-  buttonMinus = gtk_button_new_with_label("-");
-  g_signal_connect(buttonMinus, "clicked", G_CALLBACK(print_hello), NULL);
-
-  label = gtk_label_new("0");
-
-  gtk_box_append(GTK_BOX(box), buttonPlus);
-  gtk_box_append(GTK_BOX(box), buttonMinus);
-  gtk_box_append(GTK_BOX(box), label);
-
+  for (int i = 0; i < 10; i++)
+  {
+    for (int j = 0; j < 10; j++)
+    {
+      char s[10];
+      sprintf(s, "  %d  ", i * j);
+      gtk_grid_attach(GTK_GRID(grid), gtk_text_new_with_buffer(gtk_entry_buffer_new(s, 10)), i, j, 1, 1);
+    }
+  }
+  gtk_window_set_child(GTK_WINDOW(window), grid);
   gtk_window_present(GTK_WINDOW(window));
 }
 
